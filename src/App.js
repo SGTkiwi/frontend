bf49b77449dcc9fcc4a901fff78b4737661a623b
Jaehoon <joh327@aucklanduni.ac.nz>
2023-10-02 12:16:54 +1300

    [feat] added Appbar and changed background color

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {Link, Route, Routes} from 'react-router-dom';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { Link, Route, Routes } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import Jobs from "./components/jobpage/Jobs";
 import { Apple } from "@mui/icons-material";
 import Application from "./components/applicationpage/Application";
 import Profile from "./components/profilepage/Profile";
-
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 
 function App() {
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Jobs</Link>
-                    </li>
-                    <li>
-                        <Link to="/application">Application</Link>
-                    </li>
-                    <li>
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Jobs/>} />
-                <Route path="/application" element={<Application/>} />
-                <Route path="/profile" element={<Profile/>} />
-            </Routes>
-        </>
-    );
+  return (
+    <Container className="website-container">
+      <AppBar position="static">
+        <Container>
+          <Toolbar>
+            <Button color="inherit" component={Link} to="/">
+              Jobs
+            </Button>
+            <Button color="inherit" component={Link} to="/application">
+              Applications
+            </Button>
+            <Button color="inherit" component={Link} to="/profile">
+              Applications
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<Jobs />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
