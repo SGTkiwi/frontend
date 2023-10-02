9cc2b98bec251370d168528ba31ab5d928e01711
Jaehoon <joh327@aucklanduni.ac.nz>
2023-10-02 13:14:25 +1300

    feat/added underline&bold feature for buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { NavLink, Link, Route, Routes, useLocation } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,21 +10,54 @@ import { Apple } from "@mui/icons-material";
 import Application from "./components/applicationpage/Application";
 import Profile from "./components/profilepage/Profile";
 import { AppBar, Button, Container, Toolbar } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState(false);
+
+  const handleButton = () => {
+    setSelectedButton((prevButton) => !prevButton);
+  };
+  const location = useLocation();
+
+  const isButtonActive = (path) => {
+    return location.pathname === path;
+  };
+  const selectedLinkStyles = {
+    fontWeight: "bold", // Make text bold
+    textDecoration: "underline", // Underline text
+  };
   return (
     <Container className="website-container">
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{ background: "transparent", boxShadow: "none" }}
+      >
         <Container>
           <Toolbar>
-            <Button color="inherit" component={Link} to="/">
+            <Button
+              sx={{ textTransform:"none",fontSize:20, fontFamily:"Poppins", color: "#484640" }}
+              component={NavLink}
+              to="/"
+              className={isButtonActive("/") ? "selected-link" : ""}
+            >
               Jobs
             </Button>
-            <Button color="inherit" component={Link} to="/application">
+            <Button
+              sx={{ textTransform:"none", fontSize: 20, fontFamily: "Poppins", color: "#484640" }}
+              component={NavLink}
+              to="/application"
+              className={isButtonActive("/application") ? "selected-link" : ""}
+            >
               Applications
             </Button>
-            <Button color="inherit" component={Link} to="/profile">
-              Applications
+            <Button
+              sx={{ textTransform:"none", ml:80, fontSize: 20, fontFamily: "Poppins", color: "#484640" }}
+              component={NavLink}
+              to="/profile"
+              className={isButtonActive("/profile") ? "selected-link" : ""}
+            >
+              Profile
             </Button>
           </Toolbar>
         </Container>
